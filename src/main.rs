31d4aa9113cd0260e3ec43316945bad0d90a35ad
xanderwessels <xanwes97@hotmail.com>
2020-08-29 20:24:17 +0200

    changed settings

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use actix_web::{get, web, App, HttpServer, Responder};
 
-#[get("/{id}/{name}/index.html")]
-async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
-    format!("Hello {}! id:{}", name, id)
+#[get("/")]
+async fn static_files() -> impl Responder {
+    format!("Static files")
+}
+
+#[get("/api/articles/")]
+async fn articles() -> impl Responder {
+    format!("Collection of all articles(titles/dates/etc)")
+}
+
+#[get("/api/articles/{id}")]
+async fn article(web::Path(id): web::Path<u32>) -> impl Responder {
+    format!("article with id:{}", id)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| App::new().service(static_files).service(articles).service(article))
         .bind("127.0.0.1:8080")?
         .run()
         .await
